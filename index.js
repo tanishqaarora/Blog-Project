0550471988636f2c548e56844108359b69f504ac
Tanishqa Arora <tanishqa164@gmail.com>
2023-06-19 12:53:10 +0530

    image upload using multer

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 const app = express();
-const blogRoutes = require('./routes/blog');
 const blogCategoryRoutes = require('./routes/blogCategory');
 const reactionCategoryRoutes = require('./routes/reactionCategory');
+const blogRoutes = require('./routes/blog');
 const reactionRoutes = require('./routes/reaction');
 const commentRoutes = require('./routes/comment');
 const userRoutes = require('./routes/user');
@@ -16,9 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Adding Routes
-app.use('/api', blogRoutes);
 app.use('/', blogCategoryRoutes);
 app.use('/', reactionCategoryRoutes);
+app.use('/api', blogRoutes);
 app.use('/', reactionRoutes);
 app.use('/', commentRoutes);
 app.use('/', userRoutes);
--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const { createBlog, gettingBlogs, getBlog, updateBlog, deleteBlog} = require('../controllers/blog.js');
+const multer = require('multer');
 
+//Setting storage engine
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+    //   console.log(file);
+      cb(null, `${Date.now()}.${file.originalname.split('.').pop()}`);
+    },
+  });
+
+  //initializing multer
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: '1000000'},
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|jpg|png|gif/
+        const mimeType = fileTypes.test(file.mimetype)
+        const extname = fileTypes.test(file.originalname.split('.').pop());
+        if(mimeType && extname) {
+            return cb(null, true)
+        }
+        cb('Give proper image format to upload');
+    }
+  });
 
 // Creating new Blog 
-router.post('/create-blog', createBlog);
+router.post('/create-blog', upload.single('image'), createBlog);
 
 // Getting all blogs
 router.get('/get-all-blogs', gettingBlogs);
